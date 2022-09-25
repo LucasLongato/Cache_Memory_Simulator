09b6849ea9210d1ba579062a1de9bb54f92a133a
Lucas Henrique Longato Rodrigues <81321261+LucasLongato@users.noreply.github.com>
2022-09-24 22:00:45 -0300

    Add files via upload

---
+++ Arquitetura de softawer/cache.c
@@ -0,0 +1,15 @@
+//simulador de memoria cache
+//fifo 
+
+
+#include "cache.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+
+int main(){
+    
+}
