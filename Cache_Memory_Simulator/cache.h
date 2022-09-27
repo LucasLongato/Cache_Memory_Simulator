d6ee00d11953611f6986b73e7de34b142819e869
Leonardo Albano <84484869+Leonardo-Albano@users.noreply.github.com>
2022-09-26 22:09:03 -0300

    feature/Leo/1
    Corrigidos bugs da versão.

---
+++ Cache_Memory_Simulator/cache.h
@@ -0,0 +1,29 @@
+// Associatividade: 4
+// => Política de substituição: fifo
+// => Mecanismo de coerência de escritas: WB
+// => Política de alocação durante escritas: WNA
+// meu codig
+//simulador de memoria cache metodo fifo associativo wb wna
+// bloco de cache
+typedef struct Cache {
+    int tag;
+    int isValid;
+    int isDirty;
+    int data;
+} Cache;
+
+// estrutura de cache
+// typedef struct cache {
+//     cache_block blocks[ASSOCIABILITY];
+// } cache;
+
+void printCacheStats(Cache *listaCacheBlock);
+int checkCache(Cache *listaCacheBlock, int address);
+void initCache(Cache *listaCacheBlock);
+void printCacheStats(Cache *listaCacheBlock);
+void setIndex(Cache *listaCacheBlock, int address);
+int writeCache(Cache *listaCacheBlock, int address, int value);
+void freecache(Cache *listaCacheBlock);
+
+
+
